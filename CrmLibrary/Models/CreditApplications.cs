3c2c32366632d672d7535c9aa2b096dddb40e87b
Nasiba <nasyrova.nasiba@gmail.com>
2021-02-25 21:34:59 +0500

    created models for all tables in crmdb

---
+++ CrmLibrary/Models/CreditApplications.cs
@@ -0,0 +1,12 @@
+﻿namespace CrmLibrary.Models
+{
+    public class CreditApplications
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public string Purpose { get; set; }
+        public decimal Amount { get; set; }
+        public int Period { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
---
+++ CrmLibrary/Models/Form.cs
@@ -0,0 +1,10 @@
+﻿namespace CrmLibrary.Models
+{
+    public class Form
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int CreditHistory { get; set; }
+        public int Defaults { get; set; }
+    }
+}
